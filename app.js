e8f01af9d992eb795fc8ac11df96d891aeca6090
Nathan deGraaf <nathanddegraaf@gmail.com>
2019-12-09 19:29:36 -0700

    major changes, added sass

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const pageInfo = require('./pageInfo');
-const inquiries = require('./inquiries')
+require('dotenv').config();
 
 const app = express();
 
--- pageInfo.js
+++ pageInfo.js
@@ -7,14 +7,6 @@ const pageInfo = {
     href: 'about',
     title: 'About',
   },
-  runicfreak: {
-    href: 'runicfreak',
-    title: 'RunicFreak'
-  },
-  gallery: {
-    href: 'gallery',
-    title: 'Gallery',
-  },
   contact: {
     href: 'form',
     title: 'Contact'
